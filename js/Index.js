09c97f7d7134d6d1963b7dce797e64ffd5568e4f
chachis01 <92498335+chachis01@users.noreply.github.com>
2021-10-13 22:28:06 -0500

    Add files via upload

---
+++ js/Index.js
@@ -0,0 +1,13 @@
+ window.sr=ScrollReveal ();
+ 
+  sr.reveal('.navbar', {
+  	  duration: 3000,
+  	  origin: 'bottom',
+  	  distance: '-100px'
+  });
+
+    sr.reveal('.scroll-banner', {
+  	  duration: 3000,
+  	  origin: 'bottom',
+  	  distance: '-100px'
+  });
